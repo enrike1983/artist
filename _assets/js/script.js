6bd42320cdf625c368668fbbd1b41127434e9b96
Enrico Antonello <eantonel@gmail.com>
2016-11-30 15:08:49 +0100

    updates

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -3,3 +3,7 @@ var inView = require('in-view');
 inView('.fadeable').on('enter', function(el) {
     el.classList.add('in-view');
 });
+
+inView('.fadeable-later').on('enter', function(el) {
+    el.classList.add('in-view');
+});
